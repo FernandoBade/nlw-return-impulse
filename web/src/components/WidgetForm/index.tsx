b3506d11c414d90d7370dd4572ae2d6577e3da3d
FernandoBade <bade.fernando@gmail.com>
2022-05-06 20:17:43 -0300

    Mudança dinâmica do Header, botão de voltar, ajustes de Tailwind

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,9 +1,12 @@
-import { CloseButton } from "./CloseButton";
-import bugImageURL from "../assets/bug.svg";
-import ideaImageUrl from "../assets/idea.svg";
-import otherImageUrl from "../assets/other.svg";
+import { useState } from 'react';
+import { CloseButton } from "../CloseButton";
+import bugImageURL from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import otherImageUrl from "../../assets/other.svg";
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG: {
         title: "Problema",
         image: {
@@ -36,26 +39,31 @@ const feedbackTypes = {
 // ]
 
 
+export type FeedbackType = keyof typeof feedbackTypes;
+
+
 export function WidgetForm() {
+
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+
+    function handleRestartFeedback(){
+        setFeedbackType(null);
+    }
+
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-                <CloseButton />
-            </header>
 
-            <div className="flex py-8 gap-2 w-full-width">
-                {Object.entries(feedbackTypes).map(([key, value]) => {
-                    return (
-                    <button>
-                        <img src={value.image.source} alt={value.image.alt} />
-                        <span>{value.title}</span>
-                    </button>
-                )
-                })}
+            {!feedbackType ? (
 
+                <FeedbackTypeStep onFeedbackTypeChange = {setFeedbackType}/>
 
-            </div>
+            ) : (
+                <FeedbackContentStep
+                feedbackType={feedbackType}
+                onFeedbackRestartRequested={handleRestartFeedback} />
+            )}
 
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ por <a className="underline underline-offset-2" target="_blank" href="https://github.com/FernandoBade">Fernando Bade</a>
