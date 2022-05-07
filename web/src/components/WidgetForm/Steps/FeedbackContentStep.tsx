020e5cf324c09e8e0eae2aee0d32b6f630b451ca
FernandoBade <bade.fernando@gmail.com>
2022-05-06 22:01:46 -0300

    Inclusão dos botões no footer da aplicação

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
-import { ArrowLeft } from "phosphor-react";
-import { FeedbackType, feedbackTypes} from "..";
+import { ArrowLeft, Camera } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton"
 
 interface FeedbackContentStepProps {
@@ -18,9 +18,9 @@ export function FeedbackContentStep({
         <>
             <header>
                 <button
-                type="button"
-                className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
-                onClick={onFeedbackRestartRequested}
+                    type="button"
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                    onClick={onFeedbackRestartRequested}
                 >
                     <ArrowLeft weight="bold" className="w-4 h-4" />
                 </button>
@@ -31,9 +31,30 @@ export function FeedbackContentStep({
                 <CloseButton />
             </header>
 
-            <div className="flex py-8 gap-2 w-full-width">
+            <form className="my-4 w-full">
+                <textarea
+                    className="min-w-[304px] w-full h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Conte com detalhes o que esta acontecendo:"
+                />
+                <footer className="flex gap-2 mt-2">
 
-            </div>
+                    <button
+                        type="button"
+                        className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+                    >
+                        <Camera className="w-6 h-6" />
+                    </button>
+
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                    >
+                        Enviar feedback
+                    </button>
+                </footer>
+
+            </form>
+
         </>
     )
 }
